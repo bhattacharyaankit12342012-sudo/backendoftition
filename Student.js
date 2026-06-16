ec5c0d30999d261ccf806002218caead35995f72
bhattacharyaankit12342012-sudo <bhattacharyaankit12342012@gmail.com>
2026-06-16 16:45:06 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,88 @@
+const mongoose = require("../../db");
+
+const StudentSchema = new mongoose.Schema({
+  studentId: {
+    type: Number,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  className: {
+    type: String,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
+  father: {
+    type: String,
+    default: "",
+  },
+  mother: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    default: "Active",
+  },
+  feeAmount: {
+    type: Number,
+    default: 0,
+  },
+  parentEmail: {
+    type: String,
+    default: "",
+  },
+  lastPaymentDate: {
+    type: Date,
+    default: null,
+  },
+  nextPaymentDate: {
+    type: Date,
+    default: null,
+  },
+  lastReminderSentAt: {
+    type: Date,
+    default: null,
+  },
+  feeBalance: {
+    type: Number,
+    default: 0,
+  },
+  photo: {
+    type: String,
+    default: "",
+  },
+  subjects: {
+    type: [String],
+    default: [],
+  },
+  classDaysPerWeek: {
+    type: String,
+    default: "",
+  },
+  preferredTeachingTime: {
+    type: String,
+    default: "",
+  },
+  timetable: {
+    type: String,
+    default: "",
+  },
+  suspendedFrom: {
+    type: [Date],
+    default: [],
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model("Student", StudentSchema);
